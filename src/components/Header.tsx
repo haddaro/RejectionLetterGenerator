0eb4eb9d6bacd5b072dd5f7c4534747ec8fbf2ca
Haddar Beiser <haddaro@gmail.com>
2023-10-01 15:27:42 +0300

    padding in the header and colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,17 @@ const Header = () => {
         background: "linear-gradient(45deg, #B0BEC5 30%, #2196F3 90%)",
         borderRadius: "15px",
         boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.1)",
+        padding: "16px",
       }}
     >
-      <Typography variant="h3" color="Primary">
-        Rejection made easy 🫤
+      <Typography
+        variant="h3"
+        color="white"
+        style={{
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        Rejection made easy 📃➡️🗑️
       </Typography>
     </Box>
   );
